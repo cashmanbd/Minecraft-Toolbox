2e14b7a44a002ed34158ce4f14e9519b48ae85a7
cashmanbd <cashmanbd@gmail.com>
2015-03-15 15:22:57 -0400

    Updated brick colors.

--- src/matlab/plotChunk.m
+++ src/matlab/plotChunk.m
@@ -1,8 +1,7 @@
-function [ ] = plotChunk( chunk_data )
+function plotChunk( chunk_data )
     
     dimensions = size(chunk_data);
     for i=1:dimensions(1)
-        %plotPoint(chunk_data,i,1,1);
         for ii=1:dimensions(2)
             for iii=1:dimensions(3)
                 plotPoint(chunk_data,i,ii,iii);
@@ -15,36 +14,30 @@ function [ ] = plotChunk( chunk_data )
     axis([1 32 1 32 1 16]);
 end
 
-function a = plotPoint(chunk_data,i,j,k) 
-    x = [i, i,   i,   i+1, i+1, i+1];% i+1];
-    y = [k, k+1, k+1, k+1, k,   k];%   k+1];
-    z = [j, j,   j+1, j+1, j+1, j];%   j];
+function plotPoint(chunk_data,i,j,k) 
+    x = [i, i,   i,   i+1, i+1, i+1];
+    y = [k, k+1, k+1, k+1, k,   k];
+    z = [j, j,   j+1, j+1, j+1, j];
     
-  % x = [i, i, i; i+1, i+1, i; i+1, i+1, i; i, i, i];
-  % y = [k, k, k; k, k, k; k, k+1, k+1; k, k+1, k+1];
-  % z = [j, j+1, j; j, j+1, j+1; j+1, j+1, j+1; j+1, j+1, j];
-   
-  %  z = [j, j,   j+1, j+1; j+1,j+1, j+1, j+1; j, j+1,j+1, j];
     
-  %  x = [i, i+1, i+1, i;   i,  i+1, i+1, i;   i, i,  i,   i];
-  % y = [k, k,   k,   k;   k,  k,   k+1, k+1; k, k,  k+1, k+1];
-    
-    fill3(x, y, z, getColor(chunk_data(i,j,k)));
+    fill3(x, y, z, getColor(chunk_data(i,j,k)), 'EdgeColor', 'none');
     
 end
 
 function color = getColor(block_type) 
     switch block_type
         case 1
-            color = 'black';
+            color = [0.33, 0.33, 0.33];
+        case 3
+            color = [0.13, 0.55, 0.13];
         case 7
-            color = 'cyan';
+            color = [0.75, 0.75, 0.75];
         case 9
-            color = 'blue';
+            color = [0, 0, 1];
         case 13
-            color = 'black';
+            color = [0.43, 0.43, 0.43];
         case 15
-            color = 'black';
+            color = [0.29, 0.29, 0.29];
         otherwise
             color = 'white';
     end
